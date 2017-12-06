16b1044c85bfa4fc265e850c0dacddbe275b7adf
thynguyen96 <thy.ngtt@gmail.com>
2017-12-06 13:14:11 +0700

    first commit

---
+++ src/app/views/components/components-routing.module.ts
@@ -0,0 +1,92 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ButtonsComponent } from './buttons.component';
+import { CardsComponent } from './cards.component';
+import { FormsComponent } from './forms.component';
+import { ModalsComponent } from './modals.component';
+import { SocialButtonsComponent } from './social-buttons.component';
+import { SwitchesComponent } from './switches.component';
+import { TablesComponent } from './tables.component';
+import { TabsComponent } from './tabs.component';
+import { ThemSanPhamComponent } from './them-san-pham.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    data: {
+      title: 'Components'
+    },
+    children: [
+      {
+        path: 'buttons',
+        component: ButtonsComponent,
+        data: {
+          title: 'Buttons'
+        }
+      },
+      {
+        path: 'cards',
+        component: CardsComponent,
+        data: {
+          title: 'Cards'
+        }
+      },
+      {
+        path: 'forms',
+        component: FormsComponent,
+        data: {
+          title: 'Forms'
+        }
+      },
+      {
+        path: 'modals',
+        component: ModalsComponent,
+        data: {
+          title: 'Modals'
+        }
+      },
+      {
+        path: 'social-buttons',
+        component: SocialButtonsComponent,
+        data: {
+          title: 'Social buttons'
+        }
+      },
+      {
+        path: 'switches',
+        component: SwitchesComponent,
+        data: {
+          title: 'Switches'
+        }
+      },
+      {
+        path: 'tables',
+        component: TablesComponent,
+        data: {
+          title: 'Tables'
+        }
+      },
+      {
+        path: 'tabs',
+        component: TabsComponent,
+        data: {
+          title: 'Tabs'
+        }
+      },
+      {
+        path: 'them-san-pham',
+        component: ThemSanPhamComponent,
+        data: {
+          title: 'Thêm sản phẩm'
+        }
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class ComponentsRoutingModule {}
