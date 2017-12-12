ca53de4c77609be30ca41321c429bae80fd94ce9
thynguyen96 <thy.ngtt@gmail.com>
2017-12-12 17:51:37 +0700

    commit Register Admin

--- src/app/containers/register-layout/register-layout.component.ts
+++ src/app/containers/register-layout/register-layout.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register-layout.component.html'
 })
-export class RegisterLayoutComponent { }
+export class RegisterLayoutComponent implements OnInit {
+  formRegister: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+  }
+  ngOnInit() {
+    this.formRegister = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+      passwordConfirm: ['', Validators.required]
+    }, this.passwordMatchValidator)
+  }  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('passwordConfirm').value
+      ? null : { 'mismatch': true };
+  }
+registerAdmin(){ console.log(this.formRegister.value) }
+}
